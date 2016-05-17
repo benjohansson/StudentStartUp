aa17a67ed4bbd17aaa16c5e8fde73d5bf8fd3315
cthornlund <carolinethornlund@hotmail.com>
2016-05-17 15:02:52 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
+<html lang="en">
 <head>
-<title>Välkommen till StudentStartUp</title>
- <meta charset="utf-8"/>
- <link rel="stylesheet" href="homepage.css">
- 	<script src="homepage.js"></script>
+	<title>Välkommen till StudentStartUp</title>
+ 	<meta charset="utf-8"/>
+ 	<link rel="stylesheet" href="HomePage.css">
+ 	<script src="new 1.js"></script>
 </head>
 <body>
      <div class="header">
         <h1>Välkommen till StudentStartUp</h1>
      </div>
-	  <div class="dropdown">
-	<button onclick="myFunction()" class="dropbtn">Meny</button>
+	<div class="dropdown">
+	<button onclick="myFunction()" class="menybtn">Meny</button>
 	<div id="myDropdown" class="dropdown-content">
-    <a href="#home">Om oss</a>
-    <a href="#about">Hjälp</a>
-    <a href="#contact">Kontakta oss</a>
-	<a href="#conditions">Våra villkor</a>
-  </div>
-</div>
+    <a href="#OmOss">Om oss</a>
+    <a href="#hjalp">Hjälp</a>
+    <a href="#kontakt">Kontakta oss</a>
+	<a href="#villkor">Våra villkor</a>
+	</div>
+	</div>
 </body>
 </html>
