fa24341fd5c64212e5458a65e4bef1162258f770
JoakimLangelaar <joakimlangelaar@hotmail.com>
2016-05-19 11:24:16 +0200

    Update login-process.php

--- login-process.php
+++ login-process.php
@@ -13,6 +13,7 @@ $result = $mysqli->query($databaseEmail);
 $row = mysqli_fetch_row($result);
 $salt = $row[0];
 
+
 $password = isset ($_POST["logPassword"]) ? $_POST["logPassword"] : '';
 $hashpassword = ("SELECT password FROM registrate WHERE email ='$inputEmail'");
 $result2 = $mysqli->query($hashpassword);
@@ -21,16 +22,21 @@ $finalpassword = $finalpassword[0];
 
 $checkpassword = md5($password . $salt);
 
+$username = ("SELECT name FROM registrate WHERE email = '$inputEmail'");
+$nameresult = $mysqli->query($username);
+$nameaccess = mysqli_fetch_array($nameresult);
+
 if($finalpassword == $checkpassword)
 {
   session_start();
   $_SESSION['user'] = $inputEmail;
+  $_SESSION['username'] = $nameaccess;
   echo "Inloggning lyckades! " . $inputEmail;
-  //echo "<script>setTimeout(\"location.href = 'http://localhost:8888/Labb1/';\",1000);</script>";
+  echo "<script>setTimeout(\"location.href = 'homepage.php';\",1000);</script>";
 }
 else
 {
   echo "Fel lösenord eller mail";
-  //echo "<script>setTimeout(\"location.href = 'http://localhost:8888/Labb1/labb2_Joakim/login.php';\",1000);</script>";
+  echo "<script>setTimeout(\"location.href = 'login-process.php;\",1000);</script>";
 }
 ?>
