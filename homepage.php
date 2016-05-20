5e4b2e7627f22b02dfaecd66d255ce5021a5efa8
JoakimLangelaar <joakimlangelaar@hotmail.com>
2016-05-20 11:08:20 +0200

    Update and rename bootstrapTest.html to homepage.php

--- homepage.php
+++ homepage.php
@@ -1,18 +1,20 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>-Student Start Up</title>
+    <title>Student Start Up</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="sidebar.css">
+  <link rel="stylesheet" href="homepage.css">
 </head>
 <body>
 	<nav class="navbar navbar-inverse">
-	
-	
+
+
 	<div class="navbar-header">
 		<button type="button" class ="navbar-toggle" data-toggle="collapse" data-target="#MainMeny">
 		<span class="icon-bar"></span>
@@ -20,7 +22,7 @@
 		<span class="icon-bar"></span>
 		</button>
 		<a href="#" class="navbar-brand"> Student Start Up </a>
-		
+
 	</div>
 	<div class = "collapse navbar-collapse" id="MainMeny" >
 		<ul class="nav navbar-nav">
@@ -28,7 +30,7 @@
 			<li><a a href="#">Om oss</a></li>
 			<li><a a href="#">Kontakt</a></li>
 			<li><a a href="#">FaQ</a></li>
-			
+
 			<li class="dropdown">
 				<a href ="#" class="dropdown-toggle" data-toggle="dropdown"> Profil <span class="caret"></span></a>
 				<ul class="dropdown-menu">
@@ -42,7 +44,7 @@
 		</ul>
 	</div>
 	</nav>
-	
+
     <div id="wrapper">
 
         <!-- Sidebar -->
@@ -50,11 +52,20 @@
             <ul class="sidebar-nav">
                 <li><a href="#">Start</a></li>
 				<li><a href="#">Inlägg</a></li>
-                <li><a href="#">Inställningar</a></li> 
+                <li><a href="#">Inställningar</a></li>
             </ul>
         </div>
 
         <!-- Page content -->
+        <form id="adForm" name="adForm" method="POST" action="register-ad.php">
+          <input type="text" id="search" name="search" placeholder="Sök efter poster..."><br>
+
+          <input type="text" id="namn" name="namn" placeholder="Fullständigt namn"><br>
+          <input type="text" id="rubrik" name="rubrik" placeholder="Ange rubrik"><br>
+          <textarea name="usertext" id="usertext" placeholder="Skriv en kommentar..."></textarea><br>
+          <input type="submit" value="Posta!" name="postbtn" id="postbtn">
+        </form>
+
         <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
@@ -62,15 +73,15 @@
 					<a href="#" class="btn btn-danger" id="menu-toggle">Menu</a>
 					<br>
 					<br>
-						 <div class="col-md-12" style="background-color: lightblue; padding: 5px;"> 
+						 <div class="col-md-12" style="background-color: lightblue; padding: 5px;">
 								<div class="alert alert-danger fade in">
 								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-								
+
 								<hr>
 								<h1 id="inlägg-rubrik">Söker Ekonom</h1>
 								<p>Hej jag har en bra företags ide och vill ha en student med kunskap av att starta företag.
 									<br> Kontakta mig på
-									 
+
 									<a href ="#" > MyNewCompany@live.se </a>
 									<br>
 									<a href ="#" class="btn btn-success" id="Intresserad"> Intresserad</a>
@@ -80,23 +91,32 @@
 								</p>
 								<hr>
 								</div>
-								<div class="alert alert-danger fade in">
-								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-								
-								<hr>
-								<h1 id="inlägg-rubrik">Söker Ekonom</h1>
-								<p>Hej jag har en bra företags ide och vill ha en student med kunskap av att starta företag.
-									<br> Kontakta mig på
-									 
-									<a href ="#" > MyNewCompany@live.se </a>
-									<br>
-									<a href ="#" class="btn btn-success" id="Intresserad"> Intresserad</a>
-									<a href ="#" class="btn btn-danger" id="Intresserad"> Dölj</a>
-									<br>
-									<br>
-								</p>
-								<hr>
-								</div>
+
+                <?php
+                $mysqli = new mysqli('localhost', 'root', 'root', 'projektarbete');
+                $selectSQL = "SElECT * FROM annonsTable";
+                $result = $mysqli->query($selectSQL);
+
+                while($row = $result->fetch_assoc())
+                echo'
+                  <div class="alert alert-danger fade in">
+  								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+
+  								<hr>
+  								<h1 id="inlägg-rubrik">'. $row['rubrik'] . '</h1>
+  								<p> '. $row['annons'] .'
+  									<br> Kontakta mig på
+
+  									<a href ="#" > MyNewCompany@live.se </a>
+  									<br>
+  									<a href ="#" class="btn btn-success" id="Intresserad"> Intresserad</a>
+  									<a href ="#" class="btn btn-danger" id="Intresserad"> Dölj</a>
+  									<br>
+  									<br>
+  								</p>
+  								<hr>
+                </div>'
+                ?>
 						</div>
                     </div>
                 </div>
