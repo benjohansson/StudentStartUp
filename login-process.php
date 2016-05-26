6c88f24a8e2103f3b8e93b7478360d39584b03e0
cthornlund <carolinethornlund@hotmail.com>
2016-05-26 19:30:47 +0200

    Update login-process.php

--- login-process.php
+++ login-process.php
@@ -1,18 +1,15 @@
 <?php
 /*http://stackoverflow.com/questions/2235434/how-to-generate-a-random-long-salt-for-use-in-hashing*/
-$mysqli = new mysqli('localhost', 'root', 'root', 'projektarbete');
-
+$mysqli = new mysqli('dbtrain.im.uu.se', 'dbtrain_317', 'aldylb','dbtrain_317');
 if ($mysqli-> connect_error)
   {
     die("Connection failed: ".$mysqli->connect_error);
   }
-
 $inputEmail = isset ($_POST["logEmail"]) ? $_POST["logEmail"] : '';
 $databaseEmail = "SELECT salt FROM registrate WHERE email = '$inputEmail'";
 $result = $mysqli->query($databaseEmail);
 $row = mysqli_fetch_row($result);
-$salt = $row[0];
-
+$salt = $row[0]; 
 
 $password = isset ($_POST["logPassword"]) ? $_POST["logPassword"] : '';
 $hashpassword = ("SELECT password FROM registrate WHERE email ='$inputEmail'");
@@ -21,7 +18,6 @@ $finalpassword = mysqli_fetch_array($result2);
 $finalpassword = $finalpassword[0];
 
 $checkpassword = md5($password . $salt);
-
 $username = ("SELECT name FROM registrate WHERE email = '$inputEmail'");
 $nameresult = $mysqli->query($username);
 $nameaccess = mysqli_fetch_array($nameresult);
@@ -31,7 +27,7 @@ if($finalpassword == $checkpassword)
   session_start();
   $_SESSION['user'] = $inputEmail;
   $_SESSION['username'] = $nameaccess;
-  echo "Inloggning lyckades! " . $inputEmail;
+  echo "Du loggas in som: " . $inputEmail;
   echo "<script>setTimeout(\"location.href = 'homepage.php';\",1000);</script>";
 }
 else
