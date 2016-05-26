94bb82757b5cc4cce823a52858de1aaaa7d4b631
cthornlund <carolinethornlund@hotmail.com>
2016-05-26 19:28:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   <body>
     <div class="mainBorder">
       <div class="header">
-	  <img src="img/StudentStartUp.jpg" />        
+	  <h1>Välkommen till Student Start Up</h1>       
       </div>
       <div class="logInClass">
         <form id="logIn" name="logIn" method="POST" action="login-process.php">
@@ -22,38 +22,21 @@
         <fieldset>
           <legend>Inte registrerad? Registrera dig här!</legend>
           <form id="postFrame" onsubmit="return validate()" name="registerForm" method="POST" action="register-process.php">
-            <input type="text" id="regnamn" name="regnamn" placeholder="Fullständigt namn">
+            <input type="text" id="regnamn" name="regnamn" placeholder="Fullständigt namn"><br><br>
 
-            <select name="program" id="program" placeholder="Område" method="POST" action="register-process.php">
-              <option value="ekonomi">Ekonomi</option>
-              <option value="läkare">Läkare</option>
-              <option value="datorvetenskap">Datorvetenskap</option>
-              <option value="systemvetenskap">Systemvetenskap</option>
-            </select><br><br>
 
-            <input type="email" id="regmail" name="regmail" placeholder="E-post">
-            <select name="school" id="school" placeholder="Universitet" method="POST" action="register-process.php">
-              <option value="Uppsala universitet">Uppsala univeritet</option>
-              <option value="KTH">KTH</option>
-              <option value="Stockholms universitet">Stockholms universitet</option>
-              <option value="Chalmers">Chalmers</option>
-            </select><br><br>
+            <input type="email" id="regmail" name="regmail" placeholder="E-post"><br><br>
 
-            <input type="password" id="reglösenord" name="reglösenord" placeholder="Välj lösenord"></textarea><br><br>
 
-            <input type="password" id="valLösen" name="valLösen" placeholder="Upprepa lösenord"></textarea><br>
+            <input type="password" id="reglösenord" name="reglosenord" placeholder="Välj lösenord"></textarea><br><br>
+
+            <input type="password" id="valLösen" name="valLosen" placeholder="Upprepa lösenord"></textarea><br>
 
             <input type="submit" value="Registrera dig!" name="regbtn" id="regbtn">
           </form>
         </fieldset>
       </div>
-      <div class="navigation">
-        <ul id="menu">
-          <li>Hjälp</li>
-          <li>Om oss</li>
-          <li>Våra villkor</li>
-          <li>Kontakta oss</li>
-      </div>
+    ´
     </div>
   </body>
 </html>
