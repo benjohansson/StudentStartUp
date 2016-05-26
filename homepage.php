9ed095d46d8779b78ed6718c2589cd103b4138d3
cthornlund <carolinethornlund@hotmail.com>
2016-05-26 19:27:57 +0200

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -3,7 +3,7 @@
 session_start();
 if(!isset($_SESSION['user']))
 {
-  header("Location: http://localhost:8888/projektarbete/");
+  header("Location: http://localhost/grupp13/index.php");
 }
 else
 {
@@ -22,46 +22,43 @@ else
 </head>
 <body>
 	<nav class="navbar navbar-inverse">
-
-
 	    <div class="navbar-header">
 		    <button type="button" class ="navbar-toggle" data-toggle="collapse" data-target="#MainMeny">
 		    <span class="icon-bar"></span>
 		    <span class="icon-bar"></span>
 		    <span class="icon-bar"></span>
 		    </button>
-        <a href="http://localhost:8888/projektarbete/homepage.php" class="navbar-brand"> Student Start Up </a>
-
-      </div>
+			<a href="http://localhost/grupp13/homepage.php" class="navbar-brand"> Student Start Up </a>
+        </div>
     	<div class = "collapse navbar-collapse" id="MainMeny" >
     		<ul class="nav navbar-nav">
-    			<li class="active"><a a href="http://localhost:8888/projektarbete/homepage.php">Home</a></li>
-    			<li><a href="http://localhost/grupp13/homepage_om.php">Om oss</a></li>
-    			<li><a href="http://localhost/grupp13/homepage_kontakt.php">Kontakt</a></li>
-    			<li><a href="http://localhost/grupp13/homepage_faq.php#">FaQ</a></li>
-
+    			<li class="active"><a a href="http://localhost/grupp13/homepage.php#">Home</a></li>
+				<li><a href="http://localhost/grupp13/homepage_om.php">Om oss</a></li>
+				<li><a href="http://localhost/grupp13/homepage_kontakt.php">Kontakt</a></li>
+				<li><a href="http://localhost/grupp13/homepage_faq.php#">FaQ</a></li>
 			    <li class="dropdown">
-				    <a href ="#" class="dropdown-toggle" data-toggle="dropdown"> Profil <span class="caret"></span></a>
-				    <ul class="dropdown-menu">
-					    <li><a href="#">Inlägg</a></li>
-					    <li><a href="#">Vänner</a></li>
-					    <li><a href="#">Inställningar</a></li>
-				    </ul>
-          </li>
+					<a href ="#" class="dropdown-toggle" data-toggle="dropdown"> Profil <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="http://localhost/grupp13/myadds.php">Inlägg</a></li>
+						<li><a href="#">Vänner</a></li>
+						<li><a href="#">Inställningar</a></li>
+						<li><a href="myprofile.php">Min profil</a></li>
+					</ul>
+                </li>
+				<li><a href="#">Inloggad som: <?php echo $_SESSION['user']; ?> </a></li>		
 		    </ul>
 		<ul class="nav navbar-nav navbar-right">
 			<li><a href="logout.php"> - Logga Ut - </a></li>
 		</ul>
-	</div>
+		</div>
 	</nav>
 
     <div id="wrapper">
-
         <!-- Sidebar -->
         <div id="sidebar-wrapper">
             <ul class="sidebar-nav">
-                <li><a href="#">Start</a></li>
-				<li><a href="#">Inlägg</a></li>
+                <li><a href="http://localhost/grupp13/homepage.php">Start</a></li>
+				<li><a href="http://localhost/grupp13/myadds.php">Inlägg</a></li>
                 <li><a href="#">Inställningar</a></li>
             </ul>
         </div>
@@ -70,63 +67,66 @@ else
         <div id="page-content-wrapper">
             <div class="container-fluid">
                 <div class="row">
-                    <div class="col-md-12 col-xs-12">
+                    <div class="col-md-8 col-xs-12">
 					<a href="#" class="btn btn-danger" id="menu-toggle">Menu</a>
 					<br>
-          <form id="adForm" name="adForm" method="POST" action="register-ad.php">
-            <input type="text" id="search" name="search" placeholder="Sök efter poster..."><br>
+					  <form id="adForm" name="adForm" method="POST" action="ad-process.php">
+						<input type="text" id="namn" name="namn" placeholder="Fullständigt namn"><br>
+						<input type="text" id="rubrik" name="rubrik" placeholder="Ange rubrik"><br>
+						<input type="text" id="useremail" name="useremail" value="<?php echo $_SESSION['user'];?>" readonly /><br>
+						<textarea name="usertext" id="usertext" placeholder="Skriv en annons..."></textarea><br>
+						<select name="kategori" id= "kategori">
+							<option value="" disabled selected>Välj kategori<option>
+							<option value="1">IT</option>
+							<option value="2">Ekonomi</option>
+							<option value="3">Juridik</option>
+							<option value="4">Ingenjör</option>
+						</select><br>
+						<input type="submit" value="Posta!" name="postbtn" id="postbtn">
+					  </form>
+					<hr>
+						<div class="col-md-12" style="background-color: lightgrey; padding: 5px;">
+							<form name ="sök" action="get-adds.php" method="POST">
+								<select name="search" id= "search">
+									<option disabled selected>Sortera annonser efter kategori</option>
+									<option value="1"action ="get-adds.php">IT</option>
+									<option value="2"action ="get-adds.php">Ekonomi</option>
+									<option value="3"action ="get-adds.php">Juridik</option>
+									<option value="4" action" action="get-adds.php">Ingenjör</option>
+								</select><br>
+								<input type="submit" value="SÖK!" name="searchbtn" id="searchbtn">
+							</form>
+							<br>
+							<?php
+							$mysqli = new mysqli('dbtrain.im.uu.se', 'dbtrain_317', 'aldylb','dbtrain_317');
+							$selectSQL = "SELECT * FROM annonsTable,category WHERE category.ID=annonsTable.categoryID";
+							$result = $mysqli->query($selectSQL);
+							while($row = $result->fetch_assoc())
+							{
+							$url = "intrested.php?mail=".$row['email'] . "&annons=" .$row['id'] ; 
 
-            <input type="text" id="namn" name="namn" placeholder="Fullständigt namn"><br>
-            <input type="text" id="rubrik" name="rubrik" placeholder="Ange rubrik"><br>
-            <textarea name="usertext" id="usertext" placeholder="Skriv en annons..."></textarea><br>
-            <input type="submit" value="Posta!" name="postbtn" id="postbtn">
-          </form>
-					<br>
-						 <div class="col-md-12" style="background-color: lightblue; padding: 5px;">
-								<div class="alert alert-danger fade in">
+							echo'
+							<div class="alert alert-info fade in">
 								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-
-
-								<hr>
-								<h1 id="inlägg-rubrik">Rubrik här</h1>
-								<p>Annons här.
-									<br> Kontakta mig på
-
-									<a href ="#" > MyNewCompany@live.se </a>
-									<br>
-									<a href ="#" class="btn btn-success" id="Intresserad"> Intresserad</a>
-									<a href ="#" class="btn btn-danger" id="Intresserad"> Dölj</a>
-									<br>
-									<br>
-								</p>
-								<hr>
-								</div>
-
-                <?php
-                $mysqli = new mysqli('localhost', 'root', 'root', 'projektarbete');
-                $selectSQL = "SElECT * FROM annonsTable";
-                $result = $mysqli->query($selectSQL);
-
-                while($row = $result->fetch_assoc())
-                echo'
-                  <div class="alert alert-danger fade in">
-  								<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
-
-  								<hr>
-  								<h1 id="inlägg-rubrik">'. $row['rubrik'] . '</h1>
-  								<p> '. $row['annons'] .'
-  									<br> Kontakta mig på
-
-  									<a href ="#" > MyNewCompany@live.se </a>
-  									<br>
-  									<a href ="#" class="btn btn-success" id="Intresserad"> Intresserad</a>
-  									<a href ="#" class="btn btn-danger" id="Intresserad"> Dölj</a>
-  									<br>
-  									<br>
-  								</p>
-  								<hr>
-                </div>'
-                ?>
+									<h3> 
+										'.$row['categoryName'].'
+									</h3>
+									<hr>
+										<h1 id="inlägg-rubrik">'. $row['rubrik'] . '</h1>
+										<p> '. $row['annons'] .'
+											<br> Kontakta mig på
+											<a href ="#" > '.$row['email'].'</a>
+											<br>
+											<input type="text" value='.['$email'].'>
+											<a href="'.$url.'" class="btn btn-success" id="Intresserad">Intresserad</a>
+											<a href ="#" class="btn btn-danger" id="Intresserad"> Dölj</a>
+											<br>
+											<br>
+											
+									<hr>
+							</div>';
+							}
+							?>
 						</div>
                     </div>
                 </div>
@@ -141,7 +141,5 @@ else
         });
     </script>
 </body>
-</html>
-<?php } ?>
 </html>
 <?php } ?>
